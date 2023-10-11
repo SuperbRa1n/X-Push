d4751ee8896be83c588d9e4e09064fb2b2d61f27
Tannin Rachel <tanninrachel@yinlin.wiki>
2023-10-11 20:47:31 +0800

    修改POST规则

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -1,66 +1,18 @@
-from datetime import datetime
-from flask import render_template, request
-from run import app
-from wxcloudrun.dao import delete_counterbyid, query_counterbyid, insert_counter, update_counterbyid
-from wxcloudrun.model import Counters
-from wxcloudrun.response import make_succ_empty_response, make_succ_response, make_err_response
+from flask import Flask, render_template,request,jsonify
+import json
 
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-    """
-    :return: 返回index页面
-    """
-    return render_template('index.html')
+    return "hello world!"
 
+@app.route('/IoT_H2')
+def hello_H2():
+    return "Hello,IoT_H2"
 
-@app.route('/api/count', methods=['POST'])
-def count():
-    """
-    :return:计数结果/清除结果
-    """
-
-    # 获取请求体参数
-    params = request.get_json()
-
-    # 检查action参数
-    if 'action' not in params:
-        return make_err_response('缺少action参数')
-
-    # 按照不同的action的值，进行不同的操作
-    action = params['action']
-
-    # 执行自增操作
-    if action == 'inc':
-        counter = query_counterbyid(1)
-        if counter is None:
-            counter = Counters()
-            counter.id = 1
-            counter.count = 1
-            counter.created_at = datetime.now()
-            counter.updated_at = datetime.now()
-            insert_counter(counter)
-        else:
-            counter.id = 1
-            counter.count += 1
-            counter.updated_at = datetime.now()
-            update_counterbyid(counter)
-        return make_succ_response(counter.count)
-
-    # 执行清0操作
-    elif action == 'clear':
-        delete_counterbyid(1)
-        return make_succ_empty_response()
-
-    # action参数错误
-    else:
-        return make_err_response('action参数错误')
-
-
-@app.route('/api/count', methods=['GET'])
-def get_count():
-    """
-    :return: 计数的值
-    """
-    counter = Counters.query.filter(Counters.id == 1).first()
-    return make_succ_response(0) if counter is None else make_succ_response(counter.count)
+@app.route('/post', methods=['GET','POST'])
+def post_example():
+    if request.method == 'POST':
+        data = request.get_json() # 获取POST请求中的data参数
+        return jsonify({"challenge":data["challenge"]})
