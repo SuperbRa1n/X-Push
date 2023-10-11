c0037dc30556c1838d43f50c874840e6b31414dd
Tannin Rachel <tanninrachel@yinlin.wiki>
2023-10-11 21:31:15 +0800

    继续尝试修复

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -1,14 +1,29 @@
-from flask import Flask, render_template,request,jsonify
-import json
+from datetime import datetime
+from flask import render_template, request, jsonify
+from run import app
+from wxcloudrun.dao import delete_counterbyid, query_counterbyid, insert_counter, update_counterbyid
+from wxcloudrun.model import Counters
+from wxcloudrun.response import make_succ_empty_response, make_succ_response, make_err_response
 
-app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """
+    :return: 返回index页面
+    """
     return render_template('index.html')
 
+
 @app.route('/api/count', methods=['POST'])
-def tt_data():
-    if request.method == 'POST':
-        data = request.get_json() # 获取POST请求中的data参数
-        return jsonify({"challenge":data["challenge"]}) 
+def count():
+    data = request.get_json() # 获取POST请求中的data参数
+    return jsonify({"challenge":data["challenge"]})
+
+
+@app.route('/api/count', methods=['GET'])
+def get_count():
+    """
+    :return: 计数的值
+    """
+    counter = Counters.query.filter(Counters.id == 1).first()
+    return make_succ_response(0) if counter is None else make_succ_response(counter.count)
