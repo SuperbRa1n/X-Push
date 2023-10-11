46d0f697dd92fc43c98527c77bc2b23fe881deb5
Tannin Rachel <tanninrachel@yinlin.wiki>
2023-10-11 21:07:30 +0800

    近一步修改POST

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def index():
+    return render_template('index.html')
+
+@app.route('/api/count', methods=['POST'])
+def tt_data():
     if request.method == 'POST':
         data = request.get_json() # 获取POST请求中的data参数
         return jsonify({"challenge":data["challenge"]}) 
